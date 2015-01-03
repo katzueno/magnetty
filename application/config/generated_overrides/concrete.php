6893e38408f3bc95124d84d5b2f049b1313c456d
Katz Ueno <katz@concrete5.co.jp>
2015-01-03 21:04:04 +0900

    Adding config files after install

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-01-02T16:50:24+00:00
+ *
+ * @item      misc.seen_introduction
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'site' => 'Event',
+    'version_installed' => '5.7.3',
+    'misc' => array(
+        'access_entity_updated' => 1420217414,
+        'latest_version' => '5.6.3.2',
+        'seen_introduction' => true
+    )
+);
