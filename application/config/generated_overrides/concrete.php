9a8e23f95c073b6db8e9092256f64ed644325cb5
Katz Ueno <katz@concrete5.co.jp>
2015-01-13 19:05:29 +0900

    config files added

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-01-13T11:02:10+01:00
+ *
+ * @item      misc.seen_introduction
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'ja_JP',
+    'site' => 'concrete5731 test',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1421143119,
+        'latest_version' => '5.6.3.2',
+        'seen_introduction' => true
+    )
+);
