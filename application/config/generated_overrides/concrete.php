8dfa5dae76e21378450f6a5526c2be950694734a
User Steamdesign <steamdesign@www2683.sakura.ne.jp>
2015-02-05 17:30:54 +0900

    Initial Theme file added

--- application/config/generated_overrides/concrete.php
+++ application/config/generated_overrides/concrete.php
@@ -2,20 +2,22 @@
 
 /**
  * -----------------------------------------------------------------------------
- * Generated 2015-01-13T11:02:10+01:00
+ * Generated 2015-02-05T14:12:07+09:00
  *
- * @item      misc.seen_introduction
+ * @item      misc.mobile_theme_id
  * @group     concrete
  * @namespace null
  * -----------------------------------------------------------------------------
  */
 return array(
     'locale' => 'ja_JP',
-    'site' => 'concrete5731 test',
+    'site' => 'Magnettee',
     'version_installed' => '5.7.3.1',
     'misc' => array(
-        'access_entity_updated' => 1421143119,
+        'access_entity_updated' => 1423106257,
         'latest_version' => '5.6.3.2',
-        'seen_introduction' => true
+        'seen_introduction' => true,
+        'user_timezones' => false,
+        'mobile_theme_id' => '1'
     )
 );
